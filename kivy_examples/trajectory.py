3efab33fe69f74c61d80c782bd5b8c24d0d68f2c
David Lang <35975468+LangII@users.noreply.github.com>
2019-05-23 15:25:10 -0500

    Update trajectory.py
    Still having issues with sticky collisions and jittery collisions.
    - Possible solution:  The script is designed to reverse the vector upon collision detection.  This is a problem because if the object position goes beyond the collision wall for more than a single frame then the vector will be reversed more than once.  Every frame that passes with the object position outside of the collision wall, will cause the vector to reverse.  This causes the object to be in an infinite loop, being told to go left, no right, no left, no right, no left, etc.

--- kivy_examples/trajectory.py
+++ kivy_examples/trajectory.py
@@ -1,7 +1,11 @@
 
 
 
+
+
+
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.vector import Vector
 from kivy.clock import Clock
@@ -11,8 +15,16 @@ from random import randint
 
 
 
-FPS = 60  # <-- Constant:  Frames Per Second
-LAUNCH_START_POS = (25, 25)  # <-- Constant:  Starting position for 'launch_ball()' (lower-left corner).
+#####   CONSTANTS   #####
+FPS = 60
+# LAUNCH constants.
+L_START_POS = (30, 30)
+L_VEL_MIN, L_VEL_MAX = 5, 20
+L_ROT_MIN, L_ROT_MAX = 30, 80
+# PHYSICS constants.
+DRAG = .005
+GRAVITY = .2
+ELASTICITY = .9
 
 
 
@@ -31,58 +43,76 @@ class trajectory(App):
 
 
 
-# Screen class after app class.  Contains the whole of the action of the app.
+# Screen class, only child of app class.  Contains the whole of the action of the app.
 class screen(Widget):
-    ball = ObjectProperty(None)
+    # ball = ObjectProperty(None)
 
     # Function sets up primary initiating action of script, to "throw" the ball.
     def launch_ball(self):
         # Starting position of 'ball'.
-        self.ball.center = LAUNCH_START_POS
+        self.ball.center = L_START_POS
         # Velocity and angle of 'launch_ball()'.
-        self.ball.velocity = Vector(randint(5, 10), 0).rotate(randint(20, 85))
+        self.ball.velocity = Vector(randint(L_VEL_MIN, L_VEL_MAX), 0).rotate(randint(L_ROT_MIN, L_ROT_MAX))
 
+    # The applications update-frame function.
     def update(self, dt):
+        # Currently, only action to update is the ball moving.
         self.ball.move()
 
-        if (self.ball.y < 0) or (self.ball.top > self.height):
-
-            # Apply friction at wall collision.
-            self.ball.velocity_y -= self.ball.velocity_y * .2
-            self.ball.velocity_x -= self.ball.velocity_x * .2
-
-            # Reverse velocity at wall collision.
-            self.ball.velocity_y *= -1
-
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-
-            # Apply friction at wall collision.
-            self.ball.velocity_y -= self.ball.velocity_y * .2
-            self.ball.velocity_x -= self.ball.velocity_x * .2
-
-            # Reverse velocity at wall collision.
-            self.ball.velocity_x *= -1
 
 
-
+# Child of 'screen'.
 class ball(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    # Globalizing ball's variables to be updated and maintained within app 'update' function.
+    vel_x = NumericProperty(0)
+    vel_y = NumericProperty(0)
+    velocity = ReferenceListProperty(vel_x, vel_y)
+    # collided = NumericProperty(0)                                                                                     #####
 
     def move(self):
 
-        #  Apply friction at all times. (air friction)
-        self.velocity_x -= self.velocity_x * .005
-        self.velocity_y -= self.velocity_y * .005
+        if self.y < 0:
+            self.vel_y *= - ELASTICITY
+        elif self.top > self.parent.height:
+            self.vel_y *= - ELASTICITY
+        else:
+            self.vel_y -= self.vel_y * DRAG
 
+        if self.x < 0:
+            self.vel_x *= - ELASTICITY
+        elif self.right > self.parent.width:
+            self.vel_x *= - ELASTICITY
+        else:
+            self.vel_x -= self.vel_x * DRAG
+
+        # if self.collided >= 1:  self.collided -= 1                                                                    #####
+
+        # Collision detection ... Check self coordinates for intersection with designated boundaries (parent.size).  If
+        # intersection is detected, reverse axis velocity and apply ELASTICITY.
+        # if self.collided <= 0:                                                                                        #####
+        # if self.y <= 0:
+        #     self.vel_x *= ELASTICITY
+        #     self.vel_y *= - ELASTICITY
+        # elif self.top >= self.parent.height:
+        #     self.vel_x *= ELASTICITY
+        #     self.vel_y *= - ELASTICITY
+        # if self.x <= 0:
+        #     self.vel_x *= - ELASTICITY
+        #     self.vel_y *= ELASTICITY
+        # elif self.right >= self.parent.width:
+        #     self.vel_x *= - ELASTICITY
+        #     self.vel_y *= ELASTICITY
+            # self.collided = 2                                                                                         #####
+
+        #  Apply physics of drag and gravity, then update position with updated Vector.
+        # self.vel_x -= self.vel_x * DRAG
+        # self.vel_y -= self.vel_y * DRAG
+        self.vel_y -= GRAVITY
         self.pos = Vector(* self.velocity) + self.pos
-
-        #  Apply gravity.
-        self.velocity_y -= .2
 
 
 
 # Call app.
 if __name__ == '__main__':
     trajectory().run()
+
