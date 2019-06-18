084c966e7c0455fd7075698ef3ca9a93ef890615
David Lang <35975468+LangII@users.noreply.github.com>
2019-06-18 11:35:11 -0500

    Update PhysicsEnv.py
    IT WORKS! ...
    
    Object collision works (mostly)!  It's still buggy.  Can "stick" sometimes.  Haven't seen much of a pattern to the sticking.  Seems to happen with corner collisions.
    try...
    - Nested conditionals:  Test for dynamic object being inside edge 90d before corner collision.  (Need to do this anyway for optimal performance and physics accuracy).  So, might be cause of stickiness.
    - Review correct angle adjustments.  Still not 100% certain of the accuracy of the velocity angle adjustments upon collisions.
    need to...
    - CLEAN UP

--- physics/PhysicsEnv.py
+++ physics/PhysicsEnv.py
@@ -18,8 +18,8 @@ from math import hypot
 FPS = 60
 # LAUNCH constants.
 L_START_POS = (30, 30)
-L_VEL_MIN, L_VEL_MAX = 10, 30
-L_ROT_MIN, L_ROT_MAX = 30, 80
+L_VEL_MIN, L_VEL_MAX = 10, 15
+L_ROT_MIN, L_ROT_MAX = 30, 60
 # PHYSICS constants.
 DRAG = 0.01
 GRAVITY = 0.2
@@ -55,16 +55,16 @@ class Window(Widget):  # Only child of 'PhysicsEnv'.
 		self.ball.center = L_START_POS
 		# Velocity and angle of 'launchBall()'.
 		vel = randint(L_VEL_MIN, L_VEL_MAX)
-		vel = 100
+		# vel = 20
 		rot = randint(L_ROT_MIN, L_ROT_MAX)
-		rot = 55
+		# rot = 55
 		self.ball.velocity = Vector(vel, 0).rotate(rot)
 
 	# The applications update-frame function.
 	def render(self, dt):
 		# Currently, only action to update is the ball moving.
 		self.ball.move(self.obstacle)
-		
+
 
 
 
@@ -80,7 +80,7 @@ class Obstacle(Widget):  # Child of 'Window'.
 	p4 = ReferenceListProperty(ob_right, ob_y)
 	# Reference list to pass through App.
 	points = ReferenceListProperty(p1, p2, p3, p4)
-	
+
 	# Variables for normals.																		### Have not established glitch
 	n1x, n1y = NumericProperty(0), NumericProperty(0)												#	avoidance with normals yet.
 	n2x, n2y = NumericProperty(0), NumericProperty(0)												#	So, currently not using these,
@@ -91,14 +91,14 @@ class Obstacle(Widget):  # Child of 'Window'.
 	n3 = ReferenceListProperty(n3x, n3y)
 	n4 = ReferenceListProperty(n4x, n4y)
 	normals = ReferenceListProperty(n1, n2, n3, n4)
-	
+
 	# Set positional variables.
 	def build(self):
 		self.ob_x, self.ob_y = self.x, self.y
 		self.ob_right, self.ob_top = self.right, self.top
-		
-		
-		
+
+
+
 		"""																							### Again, not using these normals
 		def getPtNorm(leg_1, origin, leg_2):														#	functions but will in the future.
 			""""""
@@ -112,23 +112,21 @@ class Obstacle(Widget):  # Child of 'Window'.
 			leg_1 = Vector(leg_1)
 			origin = Vector(origin)
 			leg_2 = Vector(leg_2)
-			
+
 			acute = False  # Working variable for when 'origin' is an internal corner.
-			
+
 			# Get the angle from the 'origin' point between 'leg_1' and 'leg_2'.
 			legs_dif = Vector(leg_2 - origin).angle(leg_1 - origin)
 			if legs_dif < 0:  acute = True  # Detect internal corner and label as 'acute'.
-			
+
 			# Get the global angle of 'leg_1' from 'origin'.
 			leg_angle = Vector(1, 0).angle(leg_1 - origin)
-			
+
 			# With 'legs_dif' and 'leg_angle' get the global angle of the 'origin' normal.
 			norm_angle = - (leg_angle - (legs_dif / 2)) + 180
 			if acute:  norm_angle += 180  # Adjust norm_angle for internal corner.
-
 			# Return an extension of 1 unit from 'origin' by calculated angle 'norm_angle'.
 			return Vector(1, 0).rotate(norm_angle) + origin
-
 		def getNormals(poly_pts):
 			""""""
 			input:	poly_pts = List of Vectors as coordinates representing a polygon.
@@ -136,34 +134,32 @@ class Obstacle(Widget):  # Child of 'Window'.
 					Vectors.
 			""""""
 			normals = []  # Initiate return variable.
-
 			# Get new list of points, extending beginning of list with 'last' item and extending end
 			# of list with 'first' item.
 			points = poly_pts[:]
 			first, last = points[0], points[len(points) - 1]
 			points.insert(0, last), points.append(first)
-			
+
 			# Convert point type to 'Vector'.
 			for index, point in enumerate(points):  points[index] = Vector(point)
-			
+
 			# With extended 'points', loop through and calculate normals with 'getPtNorm()'.
 			for index, point in enumerate(points):
 				# Do not get normal for first or last item in extended 'points' list.
 				if index == 0 or index == len(points) - 1:  continue
-				
+
 				# Get normal with 'getPtNorm()' from list of 'points'.  Because 'points' is a list
 				# of coordinates representing a polygon in clockwise positional order, to get the
 				# previous and next point of polygon just 'index - 1' and 'index + 1'.
 				normal = getPtNorm(points[index - 1], point, points[index + 1])
 				normals.append(normal)
-
 			return normals
-		
+
 		self.n1x, self.n1y = getPtNorm(self.p4, self.p1, self.p2)
 		self.n2x, self.n2y = getPtNorm(self.p1, self.p2, self.p3)
 		self.n3x, self.n3y = getPtNorm(self.p2, self.p3, self.p4)
 		self.n4x, self.n4y = getPtNorm(self.p3, self.p4, self.p1)
-		
+
 		# normals = getNormals(self.points)
 		# print(normals)
 		"""
@@ -181,31 +177,46 @@ class Ball(Widget):  # Child of 'Window'.
 		# Have to use absolute values ('abs()') of velocities to avoid "sticky" collisions.
 
 		# Check for collision with floor.
-		if self.y < 0:				self.vel_y = abs(self.vel_y) * ELASTICITY
+		if self.y < 0:                        self.vel_y = abs(self.vel_y) * ELASTICITY
 		# Check for collision with ceiling.
-		elif self.top > self.parent.height:	self.vel_y = - abs(self.vel_y) * ELASTICITY
+		elif self.top > self.parent.height:   self.vel_y = - abs(self.vel_y) * ELASTICITY
 		# If no collisions, update position without affecting velocity direction and apply gravity.
-		else:					self.vel_y -= (self.vel_y * DRAG) + GRAVITY
+		else:                                 self.vel_y -= (self.vel_y * DRAG) + GRAVITY
 		# Check for collision with left wall.
-		if self.x < 0:				self.vel_x = abs(self.vel_x) * ELASTICITY
+		if self.x < 0:                        self.vel_x = abs(self.vel_x) * ELASTICITY
 		# Check for collision with right wall.
-		elif self.right > self.parent.width:	self.vel_x = - abs(self.vel_x) * ELASTICITY
+		elif self.right > self.parent.width:  self.vel_x = - abs(self.vel_x) * ELASTICITY
 		# If no collision then update position without affecting velocity direction.
-		else:					self.vel_x -= self.vel_x * DRAG
+		else:                                 self.vel_x -= self.vel_x * DRAG
 
 		# Update position by applying new velocity with Vector.
 		self.pos = Vector(self.velocity) + self.pos
 
-		
-		
-"""   \/   UNDER CONSTRUCTION   \/   """
+
+
+#####   \/   UNDER CONSTRUCTION   \/   #############################################################
 	"""
 	These are working functions for collision with corners of 'obstacle'.  Still need to integrate
 	functions for collision with sides of 'obstacle'.  Also, need to integrate normals to avoid
 	glitch phasing and sticking.
 	"""
 
-	def pointCol(self, cx, cy, cr, px, py):  return hypot(cx - px, cy - py) <= cr
+	def pointCol(self, cx, cy, cr, px, py):
+		return hypot(cx - px, cy - py) <= cr
+
+
+	def edgeCol(self, cx, cy, cr, p1x, p1y, p2x, p2y):
+		length = hypot(p2x - p1x, p2y - p1y)
+		dot_prod = (((cx - p1x) * (p2x - p1x)) + ((cy - p1y) * (p2y - p1y))) / length ** 2
+		proj_x = p1x + (dot_prod * (p2x - p1x))
+		proj_y = p1y + (dot_prod * (p2y - p1y))
+		proj_to_p1 = hypot(proj_x - p1x, proj_y - p1y)
+		proj_to_p2 = hypot(proj_x - p2x, proj_y - p2y)
+		if not proj_to_p1 + proj_to_p2 == length:  return False
+		if hypot(proj_x - cx, proj_y - cy) <= cr:  return True
+		else:  return False
+
+
 
 	def move(self, obstacle):
 		self.windowCol()
@@ -213,7 +224,7 @@ class Ball(Widget):  # Child of 'Window'.
 		for p in obstacle.points:
 			if self.pointCol(self.center[0], self.center[1], self.size[0] / 2, p[0], p[1]):
 				# If collision is detected...
-				
+
 				# Get angle from origin 'ball.center' and points 'ball.velocity' and point
 				# collided with.
 				dif = (Vector(p) - Vector(self.center)).angle(Vector(self.velocity))
@@ -227,11 +238,37 @@ class Ball(Widget):  # Child of 'Window'.
 				if dif < 0:  self.velocity = Vector(self.velocity).rotate(refl)
 				else:  self.velocity = Vector(self.velocity).rotate(-refl)
 
-"""   /\   UNDER CONSTRUCTION   /\   """
+				break
+
+			if obstacle.points.index(p) != len(obstacle.points) - 1:  p2 = obstacle.points[obstacle.points.index(p) + 1]
+			else:  p2 = obstacle.points[0]
+
+			if self.edgeCol(self.center[0], self.center[1], self.size[0] / 2, p[0], p[1], p2[0], p2[1]):
+				vel_ang = (Vector(1, 0).angle(Vector(self.velocity)))
+				# if vel_ang < 0:  vel_ang = abs(vel_ang) + 180
+				print("vel_ang", vel_ang)
+				edge_ang = (Vector(1, 0).angle(Vector(p2) - Vector(p)))
+				print("edge_ang", edge_ang)
+				# dif = (Vector(p2) - Vector(p)).angle(Vector(self.velocity))
+				dif = (edge_ang - vel_ang)
+				print("dif", dif)
+
+				# dif = edge_ang - vel_ang
+				refl = dif * 2
+				if dif < 0:  self.velocity = Vector(self.velocity).rotate(-refl)
+				else:  self.velocity = Vector(self.velocity).rotate(refl)
+
+				break
+
+				# App.get_running_app().stop()
+
+
+#####   /\   UNDER CONSTRUCTION   /\   #############################################################
 
 
 
 
 
 # Call app.
-if __name__ == '__main__': PhysicsEnv().run()
+if __name__ == '__main__':
+	PhysicsEnv().run()
